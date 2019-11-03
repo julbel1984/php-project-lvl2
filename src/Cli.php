a81a59eb28c2b18bbc9aff675fc3720aeda61904
julbel1984 <julbel1984@yandex.ru>
2019-11-03 18:16:56 +0300

    update structure

---
+++ src/Cli.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GenDiff\Cli;
+
+use function Gendiff\Differ\genDiff;
+
+const DOC = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]
+DOC;
+
+function run()
+{
+    $args = \Docopt::handle(DOC);    
+}
